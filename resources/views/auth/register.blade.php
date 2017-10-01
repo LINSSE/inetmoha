6a5599add0f2a9a187f250797f841be58384637e
dusgab <dustingassmann@gmail.com>
2017-09-30 22:03:22 -0300

    Modificacion de vistas

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('apellido') ? ' has-error' : '' }}">
+                            <label for="apellido" class="col-md-4 control-label">Apellido</label>
+
+                            <div class="col-md-6">
+                                <input id="apellido" type="text" class="form-control" name="apellido" value="{{ old('apellido') }}" required>
+
+                                @if ($errors->has('apellido'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('apellido') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail</label>
 
@@ -59,6 +73,113 @@
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('dni') ? ' has-error' : '' }}">
+                            <label for="dni" class="col-md-4 control-label">DNI</label>
+
+                            <div class="col-md-6">
+                                <input id="dni" type="text" class="form-control" name="dni" value="{{ old('dni') }}" required maxlength="8" minlength="8" inputmode="numeric">
+
+                                @if ($errors->has('dni'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('dni') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('telefono') ? ' has-error' : '' }}">
+                            <label for="telefono" class="col-md-4 control-label">Teléfono</label>
+
+                            <div class="col-md-6">
+                                <input id="telefono" type="text" class="form-control" name="telefono" value="{{ old('telefono') }}" required maxlength="13" inputmode="numeric">
+
+                                @if ($errors->has('telefono'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('telefono') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('domicilio') ? ' has-error' : '' }}">
+                            <label for="domicilio" class="col-md-4 control-label">Domicilio</label>
+
+                            <div class="col-md-6">
+                                <input id="domicilio" type="text" class="form-control" name="domicilio" value="{{ old('domicilio') }}" required>
+
+                                @if ($errors->has('domicilio'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('domicilio') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('ciudad') ? ' has-error' : '' }}">
+                            <label for="ciudad" class="col-md-4 control-label">Ciudad</label>
+
+                            <div class="col-md-6">
+                                <input id="ciudad" type="text" class="form-control" name="ciudad" value="{{ old('ciudad') }}" required>
+
+                                @if ($errors->has('ciudad'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('ciudad') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('provincia') ? ' has-error' : '' }}">
+                            <label for="provincia" class="col-md-4 control-label">Provincia</label>
+
+                            <div class="col-md-6">
+                                <input id="provincia" type="text" class="form-control" name="provincia" value="{{ old('provincia') }}" required>
+
+                                @if ($errors->has('provincia'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('provincia') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <hr>
+                        
+                        <div class="form-group{{ $errors->has('despachante') ? ' has-error' : '' }}">
+                            <label for="despachante" class="col-md-4 control-label">Despachante</label>
+
+                            <div class="col-md-6">
+                            <select class="form-control" name="id_des" value="{{ old('id_des') }}" required>
+                                <option value="1">Gonzalez Jose</option>
+                                <option value="2">Rodriguez Luis</option>
+                            </select>
+
+                                @if ($errors->has('despachante'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('despachante') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <hr>
+
+                        <div class="form-group{{ $errors->has('representante') ? ' has-error' : '' }}">
+                            <label for="representante" class="col-md-4 control-label">Representante</label>
+                            <div class="col-md-6">
+                            <select class="form-control" name="id_rep" value="{{ old('id_rep') }}" required>
+                                <option value="1">Mendoza Jorge</option>
+                                <option value="2">Lopez Raul</option>
+                            </select>
+
+                                @if ($errors->has('representante'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('representante') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
 
